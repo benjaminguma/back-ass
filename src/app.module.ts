27f6376499c8e7d2a7a1da93825ff3e685f442b4
Benjamin Guma <benjaminguma.bg@gmail.com>
2022-07-23 04:08:52 +0100

    setting up basic environment variables and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './features/auth/auth.module';
-import { PostModule } from './features/post/post.module';
-import { PostCategoryModule } from './features/post-category/post-category.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 const Modules = [AuthModule];
 
 @Module({
-  imports: [...Modules, AuthModule, PostModule, PostCategoryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      retryAttempts: 1,
+    }),
+    ...Modules,
+  ],
 })
 export class AppModule {
   constructor() {}
